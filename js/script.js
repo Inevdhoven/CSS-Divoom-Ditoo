0341a2d24ae2d53b3bae9b92cf77f1020144d60c
Inevdhoven <inevdhoven@gmail.com>
2023-03-05 11:05:18 +0100

    Commented code

--- js/script.js
+++ js/script.js
@@ -1,13 +1,13 @@
-const rotateYSlider = document.getElementById('rotateY');
-document.documentElement.style.setProperty('--rotateY', rotateYSlider.value + 'deg');
-// edit the rotateY css variable based on the slider value
-rotateYSlider.addEventListener('input', (e) => { 
-    document.documentElement.style.setProperty('--rotateY', e.target.value + 'deg');
-});
+// const rotateYSlider = document.getElementById('rotateY');
+// document.documentElement.style.setProperty('--rotateY', rotateYSlider.value + 'deg');
+// // edit the rotateY css variable based on the slider value
+// rotateYSlider.addEventListener('input', (e) => { 
+//     document.documentElement.style.setProperty('--rotateY', e.target.value + 'deg');
+// });
 
-const rotateXSlider = document.getElementById('rotateX');
-document.documentElement.style.setProperty('--rotateX', rotateXSlider.value + 'deg');
-// edit the rotateY css variable based on the slider value
-rotateXSlider.addEventListener('input', (e) => { 
-    document.documentElement.style.setProperty('--rotateX', e.target.value + 'deg');
-});
+// const rotateXSlider = document.getElementById('rotateX');
+// document.documentElement.style.setProperty('--rotateX', rotateXSlider.value + 'deg');
+// // edit the rotateY css variable based on the slider value
+// rotateXSlider.addEventListener('input', (e) => { 
+//     document.documentElement.style.setProperty('--rotateX', e.target.value + 'deg');
+// });
